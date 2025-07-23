8f98131bfd550797db3b90d24180c676deac209e
Petr Novotný <krkabol@gmail.com>
2025-07-23 08:45:14 +0200

    include herb acronym in Pladias

--- museionExporter/export_pladias/columns/col13.py
+++ museionExporter/export_pladias/columns/col13.py
@@ -5,4 +5,11 @@ import pandas
 class Column13(BaseStep):
     _column_name = "herbář"
 
+    def compute(self) -> pandas.DataFrame:
+        def format_herbarium(row):
+            herbarium = row['Herbář']
+            text = f"herb{herbarium}"
+            return text.strip()
 
+        result = self._data.apply(format_herbarium, axis=1)
+        return pandas.DataFrame({self._column_name: result})
--- museionExporter/export_pladias/columns/col17.py
+++ museionExporter/export_pladias/columns/col17.py
@@ -6,6 +6,11 @@ class Column17(BaseStep):
     _column_name = "herbářové ID"
 
     def compute(self) -> pandas.DataFrame:
-        series = self._data["Inventární číslo"].copy()
-        return pandas.DataFrame({self._column_name: series})
+        def format_herbarium(row):
+            herbarium = row['Herbář']
+            specimen_id = row['Inventární číslo']
+            text = f"{herbarium} {specimen_id}"
+            return text.strip()
 
+        result = self._data.apply(format_herbarium, axis=1)
+        return pandas.DataFrame({self._column_name: result})
