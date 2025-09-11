3fe9d835e6a50018ddf6a759e874c3d35d42de97
Petr Novotný <krkabol@gmail.com>
2025-09-11 08:25:01 +0200

    fix JACQ date format

--- museionexporter/export_jacq/columns/col12_datum.py
+++ museionexporter/export_jacq/columns/col12_datum.py
@@ -3,8 +3,32 @@ import pandas
 
 class Column12_datum(BaseStep):
 
-    _column_name="datum"
+    _column_name="Datum"
 
     def compute(self) -> pandas.DataFrame:
-        series = self._data["Datace sběru"].copy()
-        return pandas.DataFrame({self._column_name: series})
+        datum = self._data["Datace sběru"].copy().fillna('s.d.')
+        normalized = datum.apply(self.normalize_date)
+        return pandas.DataFrame({self._column_name: normalized})
+
+    def normalize_date(self, value: str) -> str:
+        value = value.strip()
+        parts = value.split(".")
+        parts = [p for p in parts if p]  # odstraní prázdné části (např. po '2.8.1979.')
+
+        if not all(p.isdigit() for p in parts):
+            return value  # nebo "" / pd.NA
+
+        if len(parts) == 3:
+            # den.měsíc.rok
+            day, month, year = parts
+            return f"{int(year):04d}-{int(month):02d}-{int(day):02d}"
+        elif len(parts) == 2:
+            # měsíc.rok
+            month, year = parts
+            return f"{int(year):04d}-{int(month):02d}"
+        elif len(parts) == 1:
+            # rok
+            year = parts[0]
+            return f"{int(year):04d}"
+        else:
+            return value
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from museionexporter.exportTypes import ExportTypes
 from museionexporter.process_file import process_uploaded_file
 
 input_path = "test/lenka.xlsx"
-output_path = "test/output.zip"
+# output_path = "test/output.zip"
+output_path = "test/output.xlsx"
 
-process_uploaded_file(input_path, output_path, ExportTypes.DWC.value)
+process_uploaded_file(input_path, output_path, ExportTypes.JACQ.value)
